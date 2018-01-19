29221e8996d6001d826c2ccfcb04db0fd954c8a6
R3turn0927 <bjleh0927@gmail.com>
2018-01-20 02:20:47 +0900

    Mainpage Script & Design

---
+++ resources/js/index.js
@@ -0,0 +1,43 @@
+//VOLUME BAR
+//volume bar event
+var volumeDrag = false;
+$('.volume').on('mousedown', function (e) {
+    volumeDrag = true;
+    $('.sound').removeClass('muted');
+    updateVolume(e.pageX);
+});
+$(document).on('mouseup', function (e) {
+    if (volumeDrag) {
+        volumeDrag = false;
+        updateVolume(e.pageX);
+    }
+});
+$(document).on('mousemove', function (e) {
+    if (volumeDrag) {
+        updateVolume(e.pageX);
+    }
+});
+var updateVolume = function (x, vol) {
+    var volume = $('.volume');
+    var percentage;
+    //if only volume have specificed
+    //then direct update volume
+    if (vol) {
+        percentage = vol * 100;
+    } else {
+        var position = x - volume.offset().left;
+        percentage = 100 * position / volume.width();
+    }
+
+    if (percentage > 100) {
+        percentage = 100;
+    }
+    if (percentage < 0) {
+        percentage = 0;
+    }
+
+    //update volume bar and video volume
+    $('.volumeBar').css('width', percentage + '%');
+    console.log(percentage/100);
+    $("video").volume = percentage/100;
+};
