66801a70a49bc93355e5bdda5a72220a7c04dcea
R3turn0927 <bjleh0927@gmail.com>
2018-01-21 03:17:25 +0900

    스크립트 파일 구조화, 커스텀채팅영역 개발

--- resources/js/index.js
+++ resources/js/index.js
@@ -38,6 +38,26 @@ var updateVolume = function (x, vol) {
 
     //update volume bar and video volume
     $('.volumeBar').css('width', percentage + '%');
-    console.log(percentage/100);
-    $("video").volume = percentage/100;
+    console.log(percentage / 100);
+    $("video").volume = percentage / 100;
 };
+
+function setTitle(vid, api) {
+    var video_title = document.getElementById("video-title");
+
+    $.get("https://www.googleapis.com/youtube/v3/videos?id=" + vid + "&key=" + api + "&fields=items(snippet(title,liveBroadcastContent))&part=snippet", function (resp) {
+        console.log(resp.items[0].snippet.title);
+        video_title.innerHTML = resp.items[0].snippet.title;
+
+        if (resp.items[0].snippet.liveBroadcastContent !== "live") {
+            $(".youtube-chat").removeAttr("style").hide();
+            $(".CustomChatting").removeAttr("style").show();
+        }
+    });
+}
+
+
+function checkLive() {
+    var isVideo = document.getElementsByClassName("live-chat-unavailable");
+    return !isVideo;
+}
